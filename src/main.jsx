f61f35e2f248752181b82cdddcb57ebb61117d87
Your Namegayatri <your@email.com~gayatridhule09@gmail.com>
2026-06-30 15:20:23 +0530

    fix github pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
 
 ReactDOM.createRoot(
-document.getElementById('root')
+  document.getElementById("root")
 ).render(
 
-<HashRouter>
-<App/>
-</HashRouter>
+  <HashRouter>
+    <App />
+  </HashRouter>
 
-)
+);
